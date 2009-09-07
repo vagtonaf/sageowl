a73684af265dc262d1eb5380c8e71da87f005033
vagtonaf <vagtonaf@2b678106-62ad-11de-81b3-7dd168245822>
2009-09-07 05:02:34 +0000

    criacao da classe Prova

--- trunk/sageowl/src/sageowl/avaliacoes/admin.py
+++ trunk/sageowl/src/sageowl/avaliacoes/admin.py
@@ -1,7 +1,10 @@
 from sageowl.avaliacoes.models import Avaliacao
+from sageowl.avaliacoes.models import Prova
 from sageowl.avaliacoes.models import Resolucao
  
 from django.contrib import admin
 
 admin.site.register(Avaliacao)
+admin.site.register(Prova)
 admin.site.register(Resolucao)
+
