be90552eaac26df5dac0d5c133f0c412d1365a2d
vagtonaf <vagtonaf@2b678106-62ad-11de-81b3-7dd168245822>
2009-09-08 01:00:34 +0000

    Alterações dos acentos

--- trunk/sageowl/src/sageowl/avaliacoes/models.py
+++ trunk/sageowl/src/sageowl/avaliacoes/models.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 from sageowl.questoes.models import Questao
 from sageowl.pessoas.models import Avaliador
@@ -22,22 +23,22 @@ CHOICE_TIPO_PROVA=(
     ('2CHP2'      ,  '2 Chamada P2' ),
     ('2CHP3'      ,  '2 Chamada P3' ),
     ('2CHPF'      ,  '2 Chamada PF' ),
-    ('RP'      ,  'Recuperacao' ),
+    ('RP'      ,  u'Recuperação' ),
 ) 
 class Avaliacao(models.Model):
-    referencia=models.CharField(max_length=20, unique=True)
+    referencia=models.CharField(max_length=20, unique=True, verbose_name=u'Referência')
     turma=models.ForeignKey(Turma)
-    dt_avaliacao=models.DateTimeField(verbose_name='Data da Avaliacao')
-    tipoAvaliacao = models.CharField('Tipo de Avaliacao',max_length=1,choices=CHOICE_TIPO_AVALIACAO)
+    dt_avaliacao=models.DateTimeField(verbose_name=u'Data da Avaliação')
+    tipoAvaliacao = models.CharField(u'Tipo de Avaliação',max_length=1,choices=CHOICE_TIPO_AVALIACAO)
     class Meta:
-        verbose_name=u'Avaliacao'
-        verbose_name_plural=u'Avaliacoes'
+        verbose_name=u'Avaliação'
+        verbose_name_plural=u'Avaliações'
     def __unicode__(self): 
         return self.referencia    
 
 class Prova(models.Model):
     avaliacao=models.ForeignKey(Avaliacao)
-    questao=models.ForeignKey(Questao)
+    questao=models.ForeignKey(Questao, verbose_name=u'Questão')
     tipoProva = models.CharField('Tipo de Prova',max_length=10,choices=CHOICE_TIPO_PROVA)    
     class Meta:
         verbose_name=u'prova'
@@ -49,15 +50,15 @@ class Resolucao(models.Model):
     avaliador=models.ForeignKey(Avaliador)
     avaliado=models.ForeignKey(Avaliado)
     prova=models.ForeignKey(Prova)
-    resposta=models.TextField(verbose_name="Resposta do Avaliado") #Resposta da discursiva
-    obsAvaliador=models.TextField(verbose_name="Observacao do Avaliador")
-    respostaAvaliador=models.BooleanField(verbose_name="Correta", default=False) # o avaliador  resposnde se esta correta
-    notaAvaliador=models.FloatField(verbose_name="Nota do Avaliador", null=True) # o nota dada pelo avaliador da questao ex 5,2
-    dth_resolucao=models.DateTimeField(verbose_name='Data de Resolucao', auto_now_add=True)
-    dth_correcao=models.DateTimeField(verbose_name='Data de Correcao')
+    resposta=models.TextField(verbose_name='Resposta do Avaliado') #Resposta da discursiva
+    obsAvaliador=models.TextField(verbose_name=u'Observação do Avaliador')
+    respostaAvaliador=models.BooleanField(verbose_name='Correta', default=False) # o avaliador  resposnde se esta correta
+    notaAvaliador=models.FloatField(verbose_name='Nota do Avaliador', null=True) # o nota dada pelo avaliador da questao ex 5,2
+    dth_resolucao=models.DateTimeField(verbose_name=u'Data de Resolução', auto_now_add=True)
+    dth_correcao=models.DateTimeField(verbose_name=u'Data de Correção')
     class Meta:
-        verbose_name=u'Resolucao'
-        verbose_name_plural=u'Resolucoes'
+        verbose_name=u'Resolução'
+        verbose_name_plural=u'Resoluções'
     def __unicode__(self): 
         return self.resposta 
     
