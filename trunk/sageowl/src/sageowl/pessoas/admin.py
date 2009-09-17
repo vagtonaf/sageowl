5fe8ce1342c7204f2d6842b9046d76f06fa72447
vagtonaf <vagtonaf@2b678106-62ad-11de-81b3-7dd168245822>
2009-09-17 04:27:59 +0000

    problema com o ModelBase

--- trunk/sageowl/src/sageowl/pessoas/admin.py
+++ trunk/sageowl/src/sageowl/pessoas/admin.py
@@ -3,11 +3,15 @@ from sageowl.pessoas.models import Avaliador
 from sageowl.pessoas.models import Avaliado
 from sageowl.pessoas.models import Usuario
 from sageowl.pessoas.models import Grupo
-
+from sageowl.pessoas.forms import AvaliadoForm
 from django.contrib import admin
+
+class AvaliadoAdmin(admin.ModelAdmin):
+    form = AvaliadoForm
 
 admin.site.register(Pessoa)
 admin.site.register(Avaliador)
-admin.site.register(Avaliado)
+admin.site.register(Avaliado, AvaliadoAdmin)
 admin.site.register(Usuario)
 admin.site.register(Grupo)
+
